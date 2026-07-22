5f85b0679d20d9d6608fff309be8a612b58ab2c7
Sourav Yadav <souravyadav119@gmail.com>
2026-07-22 08:43:33 +0530

    Day 05 - Add Count Words solution

---
+++ - 02 JavaScript/Practice/Day -05/q5.js
@@ -0,0 +1,11 @@
+// Count Words 
+
+function countwords(str) {
+    let words = str.split(" ")
+    return  words.length;
+}
+
+
+console.log(countwords("I love javascript"));
+console.log(countwords("Hello"));
+console.log(countwords("I am sourav"));
