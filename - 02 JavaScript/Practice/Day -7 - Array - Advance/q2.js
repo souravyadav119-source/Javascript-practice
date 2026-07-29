d2d313c3b3d17de476cbbf0f9c048c70e8546d53
Sourav Yadav <souravyadav119@gmail.com>
2026-07-29 10:58:04 +0530

    Solve duplicate elements in array

--- - 02 JavaScript/Practice/Day -7 - Array - Advance/q2.js
+++ - 02 JavaScript/Practice/Day -7 - Array - Advance/q2.js
@@ -1,1 +1,30 @@
-// 
+// find duplicate elment
+
+
+function duplicateelement(arr){
+    let visited = []
+
+    for(let i=0; i < arr.length; i++){
+        if(visited.includes(arr[i])){
+            continue
+        }
+
+
+        let duplicate = 0;
+        
+        for(let j=0; j < arr.length; j++){
+          if(arr[i] === arr[j]){
+            duplicate++;
+          }           
+        } 
+
+       if(duplicate > 1){
+        visited.push(arr[i]);  
+        console.log(arr[i]);
+       }
+
+       
+    }
+}
+
+duplicateelement([1,2,3,2,4,1]);
