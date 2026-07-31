d2629ffe472bfabc90146e56bc5e45c2fc67cdbd
Sourav Yadav <souravyadav119@gmail.com>
2026-07-31 18:05:41 +0530

    Day 7: Add sum of even numbers in array solution

--- - 02 JavaScript/Practice/Day -7 - Array - Advance/q5.js
+++ - 02 JavaScript/Practice/Day -7 - Array - Advance/q5.js
@@ -1,1 +1,17 @@
-// 
+// Sum of Even Number in Array 
+
+function Evensum(arr){
+    let sum = 0;
+
+    for(let i=0; i < arr.length; i++){
+        if(arr[i] % 2 === 0){
+            sum+= arr[i];
+        }
+    }
+    return sum; 
+}
+
+console.log(Evensum([1,2,3,4,5,6]));     
+console.log(Evensum([10,15,20,25,30]));   
+console.log(Evensum([1,3,5,7]));          
+console.log(Evensum([2,4,6,8]));          
