72dd9e76f65c446e1fe5698efdd90f026dbf46bd
Sourav Yadav <souravyadav119@gmail.com>
2026-08-10 12:50:30 +0530

    Complete DOM Day 03 - Remove Specific Element

---
+++ - 02 JavaScript/DOM/Day -03-Create & Remove Elements/Q7 - Remove Specific item/script.js
@@ -0,0 +1,9 @@
+let css = document.getElementById("css");
+let removebtn = document.getElementById("removebtn");
+
+removebtn.addEventListener("click", function(){
+  if(css){
+    css.remove();
+    css = null 
+  }
+})
