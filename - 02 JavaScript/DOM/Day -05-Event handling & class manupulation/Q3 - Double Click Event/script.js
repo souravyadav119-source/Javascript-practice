d0cf06ad9f3e27f2e9823bcaa04b46f0dd49e7db
Sourav Yadav <souravyadav119@gmail.com>
2026-08-24 20:56:28 +0530

    Complete DOM Day 05 Q3 - Double Click Event

---
+++ - 02 JavaScript/DOM/Day -05-Event handling & class manupulation/Q3 - Double Click Event/script.js
@@ -0,0 +1,6 @@
+let tittle = document.getElementById("tittle");
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+    tittle.textContent = "You Double Clicked Me! 🎉"
+})
