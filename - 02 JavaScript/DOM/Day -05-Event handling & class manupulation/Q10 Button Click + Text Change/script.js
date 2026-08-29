1d3c408e67490cfe93d7fac67975722aa72d6810
Sourav Yadav <souravyadav119@gmail.com>
2026-08-29 21:28:22 +0530

    Add DOM button text change practice

---
+++ - 02 JavaScript/DOM/Day -05-Event handling & class manupulation/Q10 Button Click + Text Change/script.js
@@ -0,0 +1,9 @@
+let heading = document.getElementById("heading");
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+
+    heading.textContent = "JavaScript DOM";
+
+});
