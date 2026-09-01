7aeb40f2cc0c349936d44de3185ba52a647e0788
Sourav Yadav <souravyadav119@gmail.com>
2026-09-01 21:31:34 +0530

    Complete DOM Day 6 Q1 input value show

--- - 02 JavaScript/DOM/Day -06-Forms & Input handling/Q1 Input value/script.js
+++ - 02 JavaScript/DOM/Day -06-Forms & Input handling/Q1 Input value/script.js
@@ -3,6 +3,7 @@ let p = document.querySelector("p");
 let inp = document.getElementById("inp");
 let btn = document.getElementById("btn");
 
+
 btn.addEventListener("click", function(){
     p.textContent = inp.value;
-})
+});
