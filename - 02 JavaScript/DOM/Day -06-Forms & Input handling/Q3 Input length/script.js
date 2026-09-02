473de0291ee70f180fe62b1f8c56201616e012c4
Sourav Yadav <souravyadav119@gmail.com>
2026-09-02 13:22:05 +0530

    Complete DOM Day 6 Q3 input length

---
+++ - 02 JavaScript/DOM/Day -06-Forms & Input handling/Q3 Input length/script.js
@@ -0,0 +1,8 @@
+let h1 = document.querySelector("h1");
+let p = document.querySelector("p");
+let inp = document.getElementById("inp");
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+    p.textContent = "Length:" + inp.value.length;
+})
